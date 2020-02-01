b2f43128ee1feb3cd014a319216878852621235a
pradyumn jain <pradyumn25jain@gmail.com>
2020-02-01 07:04:20 +0530

    project1

--- code/worldometers/worldometers/spiders/countries.py
+++ code/worldometers/worldometers/spiders/countries.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -8,18 +9,35 @@ class CountriesSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        # country = response.xpath("//td/a//text()").getall()
-        # links = response.xpath("//td/a/@href").getall()
         countries = response.xpath("//td/a")
         for count in countries:
-            # name = count.xpath(".//text()").get()
+            name = count.xpath(".//text()").get()
             link = count.xpath(".//@href").get()
-
-            # yield {
-            #     'title' : name ,
-            #     'countries':link
-            # } 
-
             #link is realative url without domain and protocol
+            #absolute url is the inverse of relative url 
             absolute_url = "https://www.worldometers.info{}".format(link)
-            yield scrapy.Request(url=absolute_url)
+            #or
+            absolute_url = response.urljoin(link)
+            
+            # yield scrapy.Request(url=absolute_url)
+            #or
+            # yield response.follow(url=link)
+            yield {
+                'name':name
+            }
+            yield response.follow(url=link,callback=self.parse_country,meta={'country_name':name})
+    
+    def parse_country(self,response):
+        # logging.info(response.url) #gives th elof of the response we get back from the response url
+        name = response.request.meta['country_name']
+        rows = response.xpath("(//div[@class='table-responsive'])[1]//tbody/tr")
+        for row in rows:
+                year = row.xpath(".//td[1]/text()").get()
+                population = row.xpath(".//td[2]/strong/text()").get()
+
+                yield {
+                    "name":name,
+                    "year": year,
+                    "population":population
+                }
+
